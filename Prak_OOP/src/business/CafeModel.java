c11f6ee5d7dc06b957cc9d0ffed4e0be4090fe96
NikolaiWittkowski <niwittko122@gmail.com>
2024-11-18 10:20:15 +0100

    Update CafeModel.java
    fix

--- Prak_OOP/src/business/CafeModel.java
+++ Prak_OOP/src/business/CafeModel.java
@@ -6,59 +6,36 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import MVC.CafeControl;
-import MVC.CafeView;
-
 public class CafeModel {
-	
-	private Cafe cafe;
-	private CafeView caView;
-	
-	   public void leseAusDatei(String typ){
-	    	try {
-	      		if("csv".equals(typ)){
-	      			BufferedReader ein = new BufferedReader(new FileReader("Cafe.csv"));
-	      			String[] zeile = ein.readLine().split(";");
-	      			this.cafe = new Cafe(zeile[0], 
-	      				(zeile[1]), 
-	      				(zeile[2]), 
-	      				Boolean.parseBoolean(zeile[3]), zeile[4].split("_"));
-	      				ein.close();
-	      	  			caView.zeigeInformationsfensterAn(
-	      	  	   			"Das Cafe wurden gelesen!");
-	      		}
-	       		else{
-	       			caView.zeigeInformationsfensterAn(
-		   				"Noch nicht implementiert!");
-		   		}
-			}
-			catch(IOException exc){
-				caView.zeigeFehlermeldungsfensterAn(
-					"IOException beim Lesen!");
-			}
-			catch(Exception exc){
-				caView.zeigeFehlermeldungsfensterAn(
-					"Unbekannter Fehler beim Lesen!");
-			}
-		}
-			
-		public void schreibeCafeInCsvDatei() {
-			try {
-				BufferedWriter aus 
-					= new BufferedWriter(new FileWriter("CafeAusgabe.csv", true));
-				aus.write(cafe.gibCafeZurueck(';'));
-				aus.close();
-				caView.zeigeInformationsfensterAn(
-		   			"Die Cafe wurden gespeichert!");
-			}	
-			catch(IOException exc){
-				caView.zeigeFehlermeldungsfensterAn(
-					"IOException beim Speichern!");
-			}
-			catch(Exception exc){
-				caView.zeigeFehlermeldungsfensterAn(
-					"Unbekannter Fehler beim Speichern!");
-			}
-		}
+    private Cafe cafe;
 
+    public Cafe getCafe() {
+        return cafe;
+    }
+
+    public void setCafe(Cafe cafe) {
+        this.cafe = cafe;
+    }
+
+    public void leseAusDatei(String typ) throws IOException {
+        if ("csv".equals(typ)) {
+            try (BufferedReader ein = new BufferedReader(new FileReader("Cafe.csv"))) {
+                String[] zeile = ein.readLine().split(";");
+                this.cafe = new Cafe(zeile[0], zeile[1], zeile[2], 
+                                     Boolean.parseBoolean(zeile[3]), zeile[4].split("_"));
+            }
+        } else {
+            throw new UnsupportedOperationException("Dateityp nicht unterstützt.");
+        }
+    }
+
+    public void schreibeCafeInCsvDatei() throws IOException {
+        if (this.cafe != null) {
+            try (BufferedWriter aus = new BufferedWriter(new FileWriter("CafeAusgabe.csv", true))) {
+                aus.write(cafe.gibCafeZurueck(';'));
+            }
+        } else {
+            throw new IllegalStateException("Kein Cafe gespeichert.");
+        }
+    }
 }
