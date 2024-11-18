8a0cce26d3e645c498d0d5ea50712aae55ce2f1e
NikolaiWittkowski <niwittko122@gmail.com>
2024-11-18 10:18:56 +0100

    Update CafeControl.java
    Fix

--- Prak_OOP/src/MVC/CafeControl.java
+++ Prak_OOP/src/MVC/CafeControl.java
@@ -1,43 +1,28 @@
-package MVC;
+package ownUtil;
 
-import business.Cafe;
-import business.CafeModel;
-import javafx.stage.Stage;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 
-public class CafeControl {
+public class MeldungsfensterAnzeiger {
 	
-	private CafeView caView;
-	private CafeModel caMod;
-	private Cafe cafe;
+	private AlertType alertTyp;	
+	private String titel;
+	private String meldung;
 	
-	public CafeControl(Stage primaryStage) {
-		this.caMod = new CafeModel();
-		this.caView = new CafeView(this, caMod, primaryStage);
+	public MeldungsfensterAnzeiger(AlertType alertTyp, String titel, String meldung){
+		this.alertTyp = alertTyp;
+		this.titel = titel;
+		this.meldung = meldung;
+		if(meldung == null || "".equals(meldung)){
+			this.meldung = "Die Meldung ist nicht vorhanden.";
+		}
 	}
-	
-    void nehmeCafeAuf(){
-    	try{
-    		this.cafe = new Cafe(
-    			caView.txtName.getText(), 
-   	            caView.txtOrt.getText(),
-   	            caView.txtBeschreibung.getText(),
-   	            Boolean.parseBoolean(caView.txtMitBackwerk.getText()),
-    		    caView.txtKaffeeprodukte.getText().split(";"));
-    		caView.zeigeInformationsfensterAn("Das Cafe wurde aufgenommen!");
-       	}
-       	catch(Exception exc){
-       		caView.zeigeFehlermeldungsfensterAn(exc.getMessage());
-     	}
-    }
-   
-    void zeigeCafeAn(){
-    	if(this.cafe != null){
-    		caView.txtAnzeige.setText(
-    			this.cafe.gibCafeZurueck(' '));
-    	}
-    	else{
-    		caView.zeigeInformationsfensterAn("Bisher wurde kein Cafe aufgenommen!");
-    	}
-    }
+
+    public void zeigeMeldungsfensterAn(){
+    	Alert alert = new Alert(alertTyp);
+	    alert.setTitle(this.titel);
+	    alert.setContentText(this.meldung);
+	    alert.showAndWait();
+    }   
 
 }
