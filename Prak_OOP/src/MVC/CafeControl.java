02b6a7c678699c5c9931c064c60bc96df84c40d3
NikolaiWittkowski <niwittko122@gmail.com>
2024-11-18 14:22:34 +0100

    Update CafeControl.java
    ttt

--- Prak_OOP/src/MVC/CafeControl.java
+++ Prak_OOP/src/MVC/CafeControl.java
@@ -1,28 +1,60 @@
-package ownUtil;
+package MVC;
 
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
+import business.Cafe;
+import business.CafeModel;
+import javafx.stage.Stage;
 
-public class MeldungsfensterAnzeiger {
-	
-	private AlertType alertTyp;	
-	private String titel;
-	private String meldung;
-	
-	public MeldungsfensterAnzeiger(AlertType alertTyp, String titel, String meldung){
-		this.alertTyp = alertTyp;
-		this.titel = titel;
-		this.meldung = meldung;
-		if(meldung == null || "".equals(meldung)){
-			this.meldung = "Die Meldung ist nicht vorhanden.";
-		}
-	}
+public class CafeControl {
+    private CafeView caView;
+    private CafeModel caMod;
 
-    public void zeigeMeldungsfensterAn(){
-    	Alert alert = new Alert(alertTyp);
-	    alert.setTitle(this.titel);
-	    alert.setContentText(this.meldung);
-	    alert.showAndWait();
-    }   
+    public CafeControl(Stage primaryStage) {
+        this.caMod = new CafeModel();
+        this.caView = new CafeView(this, primaryStage);
+    }
 
+    public void nehmeCafeAuf() {
+        try {
+            Cafe cafe = new Cafe(
+                caView.getTxtName(), 
+                caView.getTxtOrt(),
+                caView.getTxtBeschreibung(),
+                Boolean.parseBoolean(caView.getTxtMitBackwerk()),
+                caView.getTxtKaffeeprodukte().split(";")
+            );
+            caMod.setCafe(cafe);
+            caView.zeigeInformationsfensterAn("Das Cafe wurde aufgenommen!");
+        } catch (Exception e) {
+            caView.zeigeFehlermeldungsfensterAn("Fehler: " + e.getMessage());
+        }
+    }
+
+    public void zeigeCafeAn() {
+        Cafe cafe = caMod.getCafe();
+        if (cafe != null) {
+            caView.setTxtAnzeige(cafe.gibCafeZurueck(';'));
+        } else {
+            caView.zeigeInformationsfensterAn("Kein Cafe verfügbar.");
+        }
+    }
+
+    public void importiereCafe(String typ) {
+        try {
+            caMod.leseAusDatei(typ);
+            zeigeCafeAn();
+            caView.zeigeInformationsfensterAn("Daten erfolgreich importiert.");
+        } catch (Exception e) {
+            caView.zeigeFehlermeldungsfensterAn("Fehler: " + e.getMessage());
+        }
+    }
+
+    public void exportiereCafe() {
+        try {
+            caMod.schreibeCafeInCsvDatei();
+            caView.zeigeInformationsfensterAn("Daten erfolgreich exportiert.");
+        } catch (Exception e) {
+            caView.zeigeFehlermeldungsfensterAn("Fehler: " + e.getMessage());
+        }
+    }
 }
+
